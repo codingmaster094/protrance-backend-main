ff8d706e38b77450dc73feac800e45fb591e1fb4
dipak-trio-spire <dipak@trio-spire.com>
2025-07-31 12:08:58 +0530

    add schema markup and SEO metaData

--- src/SEO/config.ts
+++ src/SEO/config.ts
@@ -44,61 +44,62 @@ export const SEO: Field = {
       hooks: {
         afterRead: [
           ({ siblingData }) => {
-            const meta = siblingData?.meta || {}
-
             return {
               '@context': 'https://schema.org',
-              '@type': 'LocalBusiness',
-              name: meta.title || 'PROTRANCE Sandra Kircher',
-              description: meta.description || 'Hypnose am Bodensee',
-              url: meta.canonicalUrl || 'https://protrance.de',
-              image: meta.image?.url,
-              telephone: '+49 7556 2476 007',
-              email: 'info@protrance.de',
-              address: {
-                '@type': 'PostalAddress',
-                streetAddress: 'Alte Poststr. 12',
-                addressLocality: 'Uhldingen',
-                postalCode: '88690',
-                addressCountry: 'DE',
-              },
-              openingHoursSpecification: [
+              '@graph': [
                 {
-                  '@type': 'OpeningHoursSpecification',
-                  dayOfWeek: 'Monday',
-                  opens: '00:00',
-                  closes: '00:00',
-                },
-                {
-                  '@type': 'OpeningHoursSpecification',
-                  dayOfWeek: 'Tuesday',
-                  opens: '08:00',
-                  closes: '19:00',
-                },
-                {
-                  '@type': 'OpeningHoursSpecification',
-                  dayOfWeek: 'Wednesday',
-                  opens: '08:00',
-                  closes: '12:00',
-                },
-                {
-                  '@type': 'OpeningHoursSpecification',
-                  dayOfWeek: 'Thursday',
-                  opens: '08:00',
-                  closes: '18:00',
-                },
-                {
-                  '@type': 'OpeningHoursSpecification',
-                  dayOfWeek: 'Friday',
-                  opens: '08:00',
-                  closes: '12:00 Uhr Sowie nach Vereinbarung.',
-                },
-              ],
-              additionalProperty: [
-                {
-                  '@type': 'PropertyValue',
-                  name: 'Zusätzliche Termine',
-                  value: 'sowie nach Vereinbarung',
+                  '@type': ['Organization', 'CommunityHealth'],
+                  '@id': 'https://protrance.de/#organization',
+                  name: 'Protrance',
+                  url: 'https://rauchfreidurchhypnose.de/',
+                  email: 'info@protrance.de',
+                  logo: {
+                    '@type': 'ImageObject',
+                    '@id': 'https://protrance.de/#logo',
+                    url: 'https://rauchfreidurchhypnose.de/wp-content/uploads/2021/07/1024x1024_fav_1024x1024_logomark-1.png',
+                    contentUrl:
+                      'https://rauchfreidurchhypnose.de/wp-content/uploads/2021/07/1024x1024_fav_1024x1024_logomark-1.png',
+                    caption: 'Protrance',
+                    inLanguage: 'de',
+                    width: 1024,
+                    height: 1024,
+                  },
+                  address: {
+                    '@type': 'PostalAddress',
+                    streetAddress: 'Alte Poststr. 12',
+                    addressLocality: 'Uhldingen',
+                    addressRegion: 'Baden-Württemberg',
+                    postalCode: '88690',
+                    addressCountry: {
+                      '@type': 'Country',
+                      name: 'Deutschland',
+                    },
+                  },
+                  contactPoint: {
+                    '@type': 'ContactPoint',
+                    telephone: '+49 7556 2476 007',
+                    contactType: 'customer support',
+                  },
+                  openingHours: [
+                    'Monday,Tuesday,Wednesday 09:00-18:00',
+                    'Thursday 09:00-12:00',
+                    'Friday 14:00-18:00',
+                  ],
+                  location: {
+                    '@type': 'Place',
+                    '@id': 'https://protrance.de/#place',
+                    address: {
+                      '@type': 'PostalAddress',
+                      streetAddress: 'Alte Poststr. 12',
+                      addressLocality: 'Uhldingen',
+                      addressRegion: 'Baden-Württemberg',
+                      postalCode: '88690',
+                      addressCountry: {
+                        '@type': 'Country',
+                        name: 'Deutschland',
+                      },
+                    },
+                  },
                 },
               ],
             }
